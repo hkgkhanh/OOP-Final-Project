270c57e95909031604b42beddb5be46beb663650
Nông Quốc Khánh <hkg11khanh@gmail.com>
2024-12-25 11:14:03 +0700

    add data for db

---
+++ dbData/admin.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: select * from admin
+LIMIT 0, 1000
+
+-- Date: 2024-12-25 11:11
+*/
+INSERT INTO `` (`adminID`,`password`) VALUES ('admin','1234');
---
+++ dbData/doctor.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select * from doctor
+LIMIT 0, 1000
+
+-- Date: 2024-12-25 11:12
+*/
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('10','password10','Trâm','Đặng Thùy','Khoa Cấp Cứu','0990123456','dangthuy.tram@example.com','2024-01-11');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('13','password13','Thị Hạ','Nông','Nhi','0123456789','ha.nong@gmail.com','2024-12-11');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('2','password2','Bích','Trần Thị','Khoa Ngoại','0912345678','tranthi.bich@example.com','2016-07-22');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('3','password3','Cường','Lê Quốc','Khoa Sản','0923456789','lequoc.cuong@example.com','2017-05-11');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('4','password4','Đức','Phạm Minh','Khoa Tim Mạch','0934567890','phaminh.duc@example.com','2018-02-14');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('5','password5','Hiếu','Hoàng Thị','Khoa Nhi','0945678901','hoangthi.hieu@example.com','2019-08-09');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('6','password6','Lan','Bùi Minh','Khoa Mắt','0956789012','buiminh.lan@example.com','2020-01-20');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('7','password7','Hiếu','Đỗ Thị','Khoa Răng Hàm Mặt','0967890123','dothi.hieu@example.com','2021-04-12');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('8','password8','Linh','Ngô Mai','Khoa Da Liễu','0978901234','ngomai.linh@example.com','2022-06-18');
+INSERT INTO `` (`doctorID`,`password`,`firstname`,`surname`,`faculty`,`phoneNumber`,`email`,`joinDate`) VALUES ('9','password9','Kiên','Vũ Văn','Khoa Hô Hấp','0989012345','vuvan.kien@example.com','2023-09-05');
