55e1bca6e6f38b41d2156735134988596f18412c
Phạm Thái Sơn <thaisonpham12@gmail.com>
2024-12-28 23:19:19 +0700

    fix db_int.sql

--- dbData/medicalRecord.sql
+++ dbData/medicalRecord.sql
@@ -4,4 +4,5 @@ LIMIT 0, 1000
 
 -- Date: 2024-12-25 11:13
 */
-INSERT INTO `` (`recordID`,`cccd`,`doctorID`,`diagnosis`,`treatment`,`prescription`,`dateOfVisit`,`followUpDate`,`note`,`lengthOfHospitalStay`,`paid`,`subTotalFee`) VALUES ('BA0001','001234567891','10','Viêm phổi','Uống thuốc','Thuốc ABCXYZ','2024-12-25','2024-12-31','Không có ghi chú',10,0,1000000);
+INSERT INTO medicalRecord(`recordID`,`cccd`,`doctorID`,`diagnosis`,`treatment`,`prescription`,`dateOfVisit`,`followUpDate`,`note`,`lengthOfHospitalStay`,`paid`,`subTotalFee`) VALUES ('BA0001','001234567891','10','Viêm phổi','Uống thuốc','Thuốc ABCXYZ','2024-12-25','2024-12-31','Không có ghi chú',10,0,1000000);
+
